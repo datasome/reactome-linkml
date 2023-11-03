75d7a59fe0a835a16d50178fcb5f407b1253e6f6
Robert Petryszak <info@datasome.co.uk>
2023-11-03 11:25:55 +0000

    Removed superfluous file

--- additional_class_content/curator-graph-core-classes/Person.java
+++
@@ -1,9 +0,0 @@
-    @JsonGetter("publications")
-    public List<Publication> getPublications() {
-        if (publicationAuthorList == null) return null;
-        List<Publication> rtn = new ArrayList<>();
-        for (AuthorPublication publicationAuthor : publicationAuthorList) {
-            rtn.add(publicationAuthor.getPublication());
-        }
-        return rtn;
-    }
