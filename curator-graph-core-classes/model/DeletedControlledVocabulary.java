90ff701aa9af1f73512903cdfb889daef1eb4e4f
Robert Petryszak <info@datasome.co.uk>
2024-02-12 11:32:09 +0000

    Removed superfluous directories

--- curator-graph-core-classes/model/DeletedControlledVocabulary.java
+++
@@ -1,7 +0,0 @@
-package org.reactome.server.graph.curator.domain.model;
-
-public class DeletedControlledVocabulary extends ControlledVocabulary {
-
-    public DeletedControlledVocabulary() {}
-
-}
